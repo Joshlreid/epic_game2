00bfbbfc1463dc65ff70f7f55dea5253ceee6a19
Joshlreid <m265244@usna.edu>
2023-11-29 09:24:42 -0500

    Added fonts for win, lose, and time in top left corner
    Kasey helped me out with getting a way to change font repeatedly for time

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.time
 import sys
 import random
 from settings import *
@@ -8,8 +9,10 @@ from bullet import Bullet
 
 pygame.init()
 
-#background
+# background
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+game_font = pygame.font.Font("assets/fonts/Black_Crayon.ttf", 80)
+score_font = pygame.font.Font("assets/fonts/Black_Crayon.ttf", 40)
 pygame.display.set_caption("Planes!")
 mountain = pygame.image.load("assets/images/mountain1.png").convert()
 point_mountain = pygame.image.load("assets/images/pointy_mountains.png").convert()
@@ -17,6 +20,8 @@ mountain.set_colorkey((0, 0, 0))
 point_mountain.set_colorkey((0, 0, 0))
 score = 0
 death = 0
+
+# time
 
 # moving objects
 main_plane = Plane(300, 300)
@@ -33,7 +38,9 @@ def draw_background():
     background.blit(point_mountain, (0, 432 ))
     background.blit(mountain, (250, 294))
 
+
 draw_background()
+
 
 # looking for events
 while True:
@@ -63,6 +70,11 @@ while True:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             shot.shoot(main_plane.rect.center, pygame.mouse.get_pos())
 
+    # getting time
+    screen.blit(background, (0, 0))
+    time = pygame.time.get_ticks() // 1000
+    text = score_font.render(f"{time}", True, (0, 0, 0))
+    screen.blit(text, (0, 0))
 
     main_plane.update()
     planes.update()
@@ -73,12 +85,20 @@ while True:
     if len(killed_planes) > 0:
         print(f'You have killed {score} planes!')
 
+    if score == NUM_PLANES:
+        print('You Win!')
+        win_text = game_font.render("You Win!", True, (255, 69, 0))
+        screen.blit(win_text, (160, 220))
 
 
     dead = pygame.sprite.spritecollide(main_plane, planes, False)
     death += len(dead)
+    if len(dead) > 0:
+        print('You Lose!')
+        lose_text = game_font.render("You Lose!", True, (255, 69, 0))
+        screen.blit(lose_text, (160, 220))
 
-    screen.blit(background, (0, 0))
+
     main_plane.draw(screen)
     planes.draw(screen)
     shot.draw(screen)
